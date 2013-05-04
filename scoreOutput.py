947d2324830aceae0288226e8fd07e3c429ab29c
Rafael Cosman <rafaelc@stanford.edu>
2013-05-03 17:43:34 -0700

    Added scoring.

--- scoreOutput.py
+++ scoreOutput.py
@@ -4,16 +4,32 @@ import cv2
 
 import directories
 
+def greyscale(img):
+    return np.average(img, 2)
+
+def threshold(arr, threshold):
+    arr[arr<threshold] = 0
+    arr[arr != 0] = 255
+
 def scoreOutput():
     output = directories.loadImagesInFolder(directories.output)
     groundTruth = directories.loadImagesInFolder(directories.groundTruth)
     
     for outputImage, groundTruthImage in zip(output, groundTruth):
-        outputImage = np.asarray(outputImage[:-1])
-        print(outputImage[-1])
-        exit()
-        groundTruthImage = np.asarray(groundTruthImage)
+        outputImage = np.asarray(outputImage)[0]
+        outputImage = greyscale(outputImage)
+        threshold(outputImage, 100)
+        #print(outputImage[-1])
+        print(outputImage.shape)
+        print(np.max(outputImage))
         
-        print(np.count_nonzero(outputImage != groundTruthImage))
+        groundTruthImage = np.asarray(groundTruthImage)[0]
+        groundTruthImage = greyscale(groundTruthImage)
+        print(groundTruthImage.shape)
+        
+        pixelsDifferent = np.count_nonzero(outputImage != groundTruthImage)
+        pixelsTotal = outputImage.shape[0] * outputImage.shape[1]
+        
+        print("Percent different: " + str(float(pixelsDifferent)/pixelsTotal))
         
 scoreOutput()
