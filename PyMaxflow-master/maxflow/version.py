9de703e107f4301d16ee8dd71c720782fa832172
Rafael Cosman <rafaelc@stanford.edu>
2013-05-10 14:00:27 -0700

    Deleted PyMaxflow

--- PyMaxflow-master/maxflow/version.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding:utf-8 -*-
-
-__version__ = (0,0,4)
-__version_str__ = ".".join(str(i) for i in __version__)
-__version_core__ = (3,0,1)
-__author__ = "Pablo Márquez Neila <p.mneila@upm.es>"
-__author_core__ = "Vladimir Kolmogorov"
